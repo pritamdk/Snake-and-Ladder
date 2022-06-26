4f70310afaec72327f3909ddca23037c055db57e
pritamdk <pritamdk1994@gmail.com>
2022-06-26 17:47:12 +0530

    Added : Welcome message

---
+++ com/Assignment/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.Assignment;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome to Snake and Ladder Program ");
+
+        int startPositionA = 0;
+
+    }
+}
